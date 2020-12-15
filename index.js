08168203c3bde5a39c479fc3df818d9c4e5e0b99
ItsLiam <18048508+ItsLiam@users.noreply.github.com>
2020-12-16 04:14:42 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 'use strict'
 
 if (process.env.FFMPEG_BIN) {
-  module.exports = process.env.FFMPEG_BIN
+    module.exports = process.env.FFMPEG_BIN
 } else {
-  var os = require('os')
-  var path = require('path')
+    var os = require('os')
+    var path = require('path')
 
-  var binaries = Object.assign(Object.create(null), {
-    darwin: ['x64'],
-    linux: ['x64', 'ia32', 'arm64', 'arm'],
-    win32: ['x64', 'ia32']
-  })
+    var binaries = Object.assign(Object.create(null), {
+        darwin: ['x64'],
+        linux: ['x64', 'ia32', 'arm64', 'arm'],
+        win32: ['x64', 'ia32']
+    })
 
-  var platform = process.env.npm_config_platform || os.platform()
-  var arch = process.env.npm_config_arch || os.arch()
+    var platform = process.env.npm_config_platform || os.platform()
+    var arch = process.env.npm_config_arch || os.arch()
 
-  var ffmpegPath = path.join(
-    __dirname,
-    platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg'
-  )
+    var normalized;
 
-  if (!binaries[platform] || binaries[platform].indexOf(arch) === -1) {
-    ffmpegPath = null
-  }
+    switch (platform) {
+        case 'darwin':
+            normalized = 'mac';
+            break;
+        case 'win32':
+            normalized = 'win';
+            break;
+        case 'linux':
+            normalized = 'linux';
+    }
 
-  module.exports = ffmpegPath
+    var ffmpegPath = path.join(
+        __dirname,
+        normalized === 'win' ? 'ffmpeg.exe' : 'ffmpeg'
+    )
+
+    if (!binaries[normalized] || binaries[normalized].indexOf(arch) === -1) {
+        ffmpegPath = null
+    }
+
+    module.exports = ffmpegPath
 }
